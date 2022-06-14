dd474251b290d29c3fdbb2b82155a8d3b9900277
Michael <soplomiha@gmail.com>
2022-06-14 21:32:05 +0300

    Add getting POE from the database

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,35 +1,52 @@
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
+from places.models import Place
 
 
 def show_start(request):
+    places = Place.objects.all()
+    features = []
+    for place in places:
+        features.append(
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [place.coord_lng, place.coord_lat]
+                },
+                "properties": {
+                    "title": place.title,
+                    "placeId": place.id,
+                    "detailsUrl": f"/places/{place.id}"
+                }
+            }
+        )
     geo = {
-      "type": "FeatureCollection",
-      "features": [
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.62, 55.793676]
-          },
-          "properties": {
-            "title": "Легенды Москвы",
-            "placeId": "moscow_legends",
-            "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json"
-          }
-        },
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.64, 55.753676]
-          },
-          "properties": {
-            "title": "Крыши24.рф",
-            "placeId": "roofs24",
-            "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/roofs24.json"
-          }
-        }
-      ]
+        "type": "FeatureCollection",
+        "features": features,
     }
     context = {"places": geo}
     return TemplateResponse(request, "index.html", context)
+
+
+def get_place_details(_, place_id):
+    place = get_object_or_404(Place, id=place_id)
+    place_details = {
+        "title": place.title,
+        "imgs": [img.image.url for img in place.imgs.order_by('index')],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lat": place.coord_lat,
+            "lng": place.coord_lng,
+        },
+    }
+
+    return JsonResponse(
+        place_details, 
+        json_dumps_params={
+            'indent': 2,
+            'ensure_ascii': False,
+        }
+    )
