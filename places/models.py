8d65377ce76531728b7c59a93ce1073cfb764b4c
Michael <soplomiha@gmail.com>
2022-06-22 19:38:28 +0300

    Change description fields options

--- places/models.py
+++ places/models.py
@@ -4,8 +4,10 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(max_length=150, verbose_name='Название')
-    description_short = models.TextField(verbose_name='Краткое описание')
-    description_long = HTMLField(verbose_name='Полное описание')
+    description_short = models.TextField(
+        blank=True, null=True, verbose_name='Краткое описание')
+    description_long = HTMLField(
+        blank=True, null=True, verbose_name='Полное описание')
     coord_lng = models.FloatField(verbose_name='Долгота')
     coord_lat = models.FloatField(verbose_name='Широта')
 
